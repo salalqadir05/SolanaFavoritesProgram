03ce1ecc6b8edf8f95bccdbe6be31a7ad8d983d0
Muhammad Salal Qadir <93817435+salalqadir05@users.noreply.github.com>
2025-05-27 15:41:18 +0500

    adding a first solana program for the setting up the favorites like number,color and hobbies

---
+++ programs/favorites/src/lib.rs
@@ -0,0 +1,54 @@
+use anchor_lang::prelude::*;
+
+declare_id!("Hvi4Hi6RfZwnYXUwSBuhcYZXsStUiVzCzvSWE2TdBgsK");
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites(
+        context: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
+        msg!("Greeting from {}", context.program_id);
+        let user_public_key = context.accounts.user.key();
+        msg!("User {user_public_key}");
+        context.accounts.favorites.set_inner(Favorites {
+             number,
+            color,
+            hobbies,
+        });
+        Ok(())
+    }
+}
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+
+    #[max_len(50)]
+    pub color: String,
+
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
+}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+
+    pub system_program: Program<'info, System>,
+}
